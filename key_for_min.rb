03029c98b1d710af0e97b5a1d2e40da0c6120cda
droll-framework-1384 <ebyan.yusuf@gmail.com>
2020-11-10 01:34:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-def key_for_min_value 
+def key_for_min_value(name_hash)
 min_value = nil
      min_key = nil 
 
