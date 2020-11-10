6a017606e031e16df4ba6067adf5965f613470c5
droll-framework-1384 <ebyan.yusuf@gmail.com>
2020-11-10 01:27:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  count == -1
+  count = -1
   name_hash.each do |key, value|
     name_hash[key][value] < count 
     p name_hash[key]
