23feff2466e1fb53e47ad1d8e32a6f2845d33872
droll-framework-1384 <ebyan.yusuf@gmail.com>
2020-11-10 01:33:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,8 +1,17 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-  smallest_value = 0 
-  empty_hash = nil 
-  name_hash.each do |name, value|
-   if smallest_value == 0 || 
+min_value = nil
+     min_key = nil 
+
+     name_hash.each do |key, value|
+         if min_value == nil
+             min_value = value
+             min_key = key
+         elsif min_value > value
+             min_value = value
+             min_key = key
+         end
+     end
+     min_key
+ end
